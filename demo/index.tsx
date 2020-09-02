37da77a9828313deb5f885acdefe97a42a4063cf
Matthias Giger <i@matthiasgiger.com>
2020-09-02 19:51:27 +0200

    refactor(demo): use papua for demo

---
+++ demo/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { css } from '@emotion/core'
+import { Global, Header, Navigation, Content, Footer, Element } from 'naven'
+
+render(
+  <>
+    <Global root="body" />
+    <Header title="naven Demo" />
+    <Navigation />
+    <Content>
+      <Element.Heading>naven Preview</Element.Heading>
+      <Element.Paragraph>This is an introduction.</Element.Paragraph>
+      <Element.SubHeading>naven Basics</Element.SubHeading>
+      <Element.Paragraph>
+        Here is a list of the included elements.
+      </Element.Paragraph>
+    </Content>
+    <Footer
+      rowStyle={css`
+        background: white;
+      `}
+    />
+  </>,
+  document.body
+)
