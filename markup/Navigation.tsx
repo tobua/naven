2adf49554d2c45d3579b786aaf0a79a0721d7120
Matthias Giger <i@matthiasgiger.com>
2020-11-10 16:54:21 +0100

    feat(navigation): styles for phone navigation and dependencies updated

--- markup/Navigation.tsx
+++ markup/Navigation.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { css } from '@emotion/core'
 import styled from '@emotion/styled'
 import { List, TextLink } from './Element'
 import { navigation } from '../config'
@@ -23,6 +24,20 @@ export const Aside = styled.aside`
   grid-column: 2 / 3;
 `
 
+const listStyles = css`
+  ${Breakpoint.Phone} {
+    flex: 1;
+    flex-direction: column;
+  }
+`
+
+const listElementStyles = css`
+  ${Breakpoint.Phone} {
+    padding: 0;
+    margin-bottom: ${Space.medium};
+  }
+`
+
 const Content = styled.div<{ isOpen: boolean }>`
   height: ${({ isOpen }) => (isOpen ? 'auto' : 0)};
   overflow: hidden;
@@ -31,10 +46,21 @@ const Content = styled.div<{ isOpen: boolean }>`
   top: 30px;
   left: 0;
   right: 0;
+
+  ${Breakpoint.Phone} {
+    position: relative;
+    height: auto;
+    top: inherit;
+    margin-top: ${Space.small};
+  }
 `
 
 const ContentContainer = styled.div`
   padding: ${Space.medium};
+
+  ${Breakpoint.Phone} {
+    padding: 0;
+  }
 `
 
 const TabElement = styled.div`
@@ -84,7 +110,11 @@ export const Navigation = () => {
 
   return (
     <Wrapper showNavigation={showNavigation}>
-      <List horizontal>
+      <List
+        css={listStyles}
+        elementProps={{ css: listElementStyles }}
+        horizontal
+      >
         {navigation.top.map((link, index) => (
           <Tab key={index}>
             <TextLink href={link.title.url}>{link.title.name}</TextLink>
--- markup/element/List.tsx
+++ markup/element/List.tsx
@@ -3,13 +3,6 @@ import { SerializedStyles } from '@emotion/core'
 import styled from '@emotion/styled'
 import { Space } from '../../style'
 
-type ListProps = {
-  horizontal?: boolean
-  wrap?: boolean
-  css?: SerializedStyles
-  children: any[]
-}
-
 const ListUl = styled.ul<ListProps>(
   {
     display: 'flex',
@@ -23,12 +16,28 @@ const ListUl = styled.ul<ListProps>(
 
 const ListLi = styled.li`
   padding: ${Space.small};
+  ${({ css }) => css}
 `
 
-export const List = ({ horizontal, children, ...props }: ListProps) => (
+type ListProps = {
+  horizontal?: boolean
+  wrap?: boolean
+  css?: SerializedStyles
+  children: any[]
+  elementProps?: {}
+}
+
+export const List = ({
+  horizontal,
+  children,
+  elementProps,
+  ...props
+}: ListProps) => (
   <ListUl horizontal={horizontal} {...props}>
     {children.map((child, index) => (
-      <ListLi key={index}>{child}</ListLi>
+      <ListLi key={index} {...elementProps}>
+        {child}
+      </ListLi>
     ))}
   </ListUl>
 )
