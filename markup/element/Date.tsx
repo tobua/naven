d544d4c97fe2cb52f12e6eb61421c45c67caaa79
Matthias Giger <i@matthiasgiger.com>
2020-11-14 20:20:24 +0100

    fix(element): prevent some react errors thrown in DateInput

--- markup/element/Date.tsx
+++ markup/element/Date.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, Component } from 'react'
 import { Global, css } from '@emotion/react'
 import ReactDatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
@@ -9,9 +9,16 @@ interface IDate {
   styleOverrides?: string
 }
 
-const DateInput = ({ value, onClick }: { value: any; onClick: any }) => (
-  <Input value={value} onClick={onClick} />
-)
+// Will throw an error related to refs the plugin tries to
+// attach if written as functional component.
+// eslint-disable-next-line react/prefer-stateless-function
+class DateInput extends Component<{ value: any; onClick: any }> {
+  render() {
+    const { value, onClick } = this.props
+    // Empty onChange to prevent react error, will be overridden by plugin later anyways.
+    return <Input value={value} onClick={onClick} onChange={() => {}} />
+  }
+}
 
 // Date is already reserved in JS, therefore we use DatePicker.
 export const DatePicker = ({
