6fecd76e71b29d9d8817968bcb67920bd5fa7513
Matthias Giger <i@matthiasgiger.com>
2021-06-25 10:48:01 +0200

    fix(element): center placeholder image text and improve styles

--- markup/element/Image.tsx
+++ markup/element/Image.tsx
@@ -4,7 +4,7 @@ import { SerializedStyles } from '@emotion/react'
 import { spaceProp } from '../../style'
 
 const placeholder = (width: number, height: number) =>
-  `data:image/svg+xml;utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' width='${width}' height='${height}' style='background-color: gray'%3E%3Ctext x='10' y='10' fill='black'%3E${width} x ${height}%3C/text%3E%3C/svg%3E`
+  `data:image/svg+xml;utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' width='${width}' height='${height}' style='background-color: %23EEE; font-family: sans-serif;'%3E%3Ctext x='50%' y='50%' dominant-baseline='middle' text-anchor='middle' fill='black'%3E${width} x ${height}%3C/text%3E%3C/svg%3E`
 
 const Wrapper = styled.img<{
   space?: string | number
