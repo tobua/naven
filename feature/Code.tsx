1363ff3aa18b723485fd722fc5611f21eab2ee42
Matthias Giger <i@matthiasgiger.com>
2021-07-11 18:19:17 +0200

    fix(feature): combine customStyle property with defaults

--- feature/Code.tsx
+++ feature/Code.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // @ts-ignore
 import { spaceStyleProp, radiusStyleProp } from 'naven'
+import { SyntaxHighlighterProps } from 'react-syntax-highlighter'
 import DefaultHighlighter, {
   Prism,
 } from 'react-syntax-highlighter/dist/esm/index'
@@ -21,8 +22,9 @@ export const Code = ({
   jsx = false,
   language = 'typescript',
   style,
+  customStyle,
   ...props
-}: ICode) => {
+}: ICode & SyntaxHighlighterProps) => {
   const SyntaxHighlighter = jsx ? Prism : DefaultHighlighter
   const importedStyle = jsx ? prismStyle : githubStyle
 
@@ -34,6 +36,7 @@ export const Code = ({
         ...spaceStyleProp(space),
         ...radiusStyleProp(),
         fontFamily: 'monospace, monospace',
+        ...(typeof customStyle === 'object' ? customStyle : {}),
       }}
       {...props}
     >
